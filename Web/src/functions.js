e39d879b9b47800b4b876f82afe4b7c2c29fd024
Xshinla <xuanlam10301@gmail.com>
2022-09-21 14:51:57 +0700

    lam

---
+++ Web/src/functions.js
@@ -0,0 +1,22 @@
+//den
+let denOn = document.querySelector('#denOn');
+let den = document.querySelector('#den');
+let denOff = document.querySelector('#denOff');
+
+
+denOn.addEventListener('click', ()=>{
+    den.src = '../img/den_on.jpg';
+})
+
+denOff.addEventListener('click', ()=>{
+    den.src = '../img/den_off.jpg';
+})
+
+
+
+
+
+
+
+
+
